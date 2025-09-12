32aed179bfb26615c0ee5410d59e56ed86d4e8db
Tsutomu Shinzawa <tsutomu.shinzawa320@gmail.com>
2025-09-12 10:04:01 +0900

    modify contactcontroller

--- src/app/Http/Controllers/ContactController.php
+++ src/app/Http/Controllers/ContactController.php
@@ -16,26 +16,27 @@ class ContactController extends Controller
 
     public function confirm(ContactRequest $request)
     {
-        $contact = $request->only(['last_name', 'first_name','gender', 'email', 'tel1','tel2','tel3', 'address', 'building','category', 'content']);
+        $contact = $request->only(['last_name', 'first_name', 'gender', 'email', 'tel1', 'tel2', 'tel3', 'address', 'building', 'category', 'content']);
         $categories = Category::all();
-        return view('confirm', compact('contact','categories'));
+        return view('confirm', compact('contact', 'categories'));
     }
 
     public function store(ContactRequest $request)
     {
-        $contact = $request->only(['last_name', 'first_name','gender', 'email', 'tel' , 'address', 'building', 'category', 'content']);
+        $contact = $request->only(['last_name', 'first_name', 'gender', 'email', 'tel', 'address', 'building', 'category', 'content']);
         Contact::create($contact);
         return view('thanks');
     }
 
-    public function search(Request $request) {
+    public function search(Request $request)
+    {
         $contacts = Contact::with('category')
-            ->KeywordSearch($request->keyword)->get();
-            // ->GenderSearch($request->gender)
-            // ->CategorySearch($request->category_id)
-            // ->DateSearch($request->date)->get();
+            ->KeywordSearch($request->keyword)
+            ->GenderSearch($request->gender)
+            ->CategorySearch($request->category_id)
+            ->DateSearch($request->date)->get();
         $categories = Category::all();
-        
+
         return view('admin', compact('contacts', 'categories'));
     }
 }
